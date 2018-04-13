cc3d40cdb9620ef017f098d40fca637c6f8536b7
MrCat9 <634467045@qq.com>
2018-04-13 21:58:47 +0800

    Update CrawlSpider对拉钩网进行整站爬取1_ 数据表结构设计.py

---
+++ CrawlSpider对拉钩网进行整站爬取1_ 数据表结构设计.py
@@ -0,0 +1,25 @@
+# 在数据库article_spider的tables下，新建表 lagou
+Field Name              Datatype        Len         Default     PK?     Not Null
+url                     varchar         300                                √
+url_object_id           varchar         50                       √         √
+title                   varchar         100                                √
+salary                  varchar         20
+job_city                varchar         10
+work_years              varchar         100
+degree_need             varchar         30
+job_type                varchar         20
+publish_time            varchar         20                                 √
+tags                    varchar         100
+job_advantage           varchar         1000
+job_desc                longtext                                           √
+job_addr                varchar         50
+company_url             varchar         300
+company_name            varchar         100
+crawl_time              datetime                                           √
+crawl_update_time       datetime
+
+
+
+
+
+
