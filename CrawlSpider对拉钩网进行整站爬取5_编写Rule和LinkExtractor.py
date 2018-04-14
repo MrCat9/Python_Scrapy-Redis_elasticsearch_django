8625f80370a397bd607617b093b76fd48d2e9384
MrCat9 <634467045@qq.com>
2018-04-14 23:20:43 +0800

    Update CrawlSpider对拉钩网进行整站爬取5_编写Rule和LinkExtractor.py

--- CrawlSpider对拉钩网进行整站爬取5_编写Rule和LinkExtractor.py
+++ CrawlSpider对拉钩网进行整站爬取5_编写Rule和LinkExtractor.py
@@ -1,1 +1,189 @@
+# test_scrapy_spider\test_scrapy_spider\spiders\lagou.py
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class LagouSpider(CrawlSpider):  #继承CrawlSpider  #CrawlSpider不能覆盖parse函数  # _parse_response是CrawlSpider的核心函数
+    name = 'lagou'                                 #可以选择重载parse_start_url或者process_results
+    allowed_domains = ['www.lagou.com']
+    start_urls = ['https://www.lagou.com']
+
+#     rules = (  # rules里放Rule实例  #
+#         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+#     )
+# 
+#     def parse_item(self, response):
+#         i = {}
+#         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+#         #i['name'] = response.xpath('//div[@id="name"]').extract()
+#         #i['description'] = response.xpath('//div[@id="description"]').extract()
+#         return i
+
+    rules = (  # rules里放Rule实例  #allowed_domains会先过滤一些url
+        Rule(LinkExtractor(allow=("zhaopin/.*",)), follow=True),  #https://www.lagou.com/zhaopin/Python/?labelWords=label
+        Rule(LinkExtractor(allow=("gongsi/j\d+.html",)), follow=True),  #https://www.lagou.com/gongsi/j173918.html
+        Rule(LinkExtractor(allow=r'jobs/\d+.html'), callback='parse_job', follow=True),  #https://www.lagou.com/jobs/4155435.html
+    )                           #匹配这个re的url，跳到parse_job
+    #
+    # def parse_start_url(self, response):  #这里不需要重载parse_start_url
+    #     return []
+    #
+    # def process_results(self, response, results):  #这里不需要重载process_results
+    #     return results
+
+    def parse_job(self, response):
+        #解析拉勾网的职位
+        pass  #debug看rule是否能进到parse_job
+
+
+
+# test_scrapy_spider\test_scrapy_spider\settings.py
+# -*- coding: utf-8 -*-
+
+import os  #用于获取当前文件（setting.py）的路径
+
+# Scrapy settings for test_scrapy_spider project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'test_scrapy_spider'
+
+SPIDER_MODULES = ['test_scrapy_spider.spiders']
+NEWSPIDER_MODULE = 'test_scrapy_spider.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'test_scrapy_spider (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'test_scrapy_spider.middlewares.TestJobboleSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'test_scrapy_spider.middlewares.TestJobboleDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    #'test_scrapy_spider.pipelines.JsonExporterPipeline': 2,  #数字越小，越先处理
+    #'scrapy.pipelines.images.ImagesPipeline': 1
+    #'test_scrapy_spider.pipelines.ArticleImagePipeline': 1,  #调用定制化的pipeline（ArticleImagePipeline）
+    #'test_scrapy_spider.pipelines.MysqlPipeline': 1
+    'test_scrapy_spider.pipelines.MysqlTwistedPipline': 1
+}
+IMAGES_URLS_FIELD = 'front_image_url'  #告诉images items中哪个是图片的url  #images需要接受一个数组
+#os.path.dirname(__file__)  #获取当前文件的目录名称（test_scrapy_spider）  #__file__是当前文件（setting.py）的名称
+project_dir =  os.path.abspath(os.path.dirname(__file__))  #获取当前文件的目录的路径
+IMAGES_STORE = os.path.join(project_dir, 'images')  #图片下载的保存路径  可以配置为绝对路径  要存在工程目录下，可以使用相对路径。在settings.py的同级目录下新建images
+                                                    #图片储存在 project_dir目录下的images文件夹
+                                                    #要下载图片需要PIL库
+                                                    #下cmd下安装PIL库
+                                                    #pip install -i https://pypi.douban.com/simple pillow
+# IMAGES_MIN_HEIGHT = 100 #设置下载图片的最小高度  #过滤图片可以在settng.py中设置
+# IMAGES_MIN_WIDTH = 100
+# '''如果要实现自己的需求，也可以重载相应的函数达到需求，在pipelines中建立类，继承ImagesPipeline就可以了'''
+
+# 在F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider\settings.py 下，设置F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider 为根目录
+# 因为虽然在eclipse里已经将 F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider 添加到path里了，但在cmd下还未添加
+import sys  #import os  在上面写过了
+# sys.path.insert(0, "F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider")  #将F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider 添加到path
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))  #F:\eclipse\···\···\test_scrapy_spider  工程路径
+sys.path.insert(0, os.path.join(BASE_DIR, 'test_scrapy_spider'))  #将F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider 添加到path里
+             #放在第0个，会优先在F:\eclipse\···\···\test_scrapy_spider\test_scrapy_spider里找需要import的module
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MYSQL_HOST = "127.0.0.1"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "123456"
+MYSQL_DBNAME = "article_spider"
+
+SQL_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+SQL_DATE_FORMAT = "%Y-%m-%d"
+
+
+
+# test_scrapy_spider\main.py
+# -*- coding: utf-8 -*-
+
+from scrapy.cmdline import execute  #调用这个函数可以执行scrapy的脚本
+
+# import sys
+# sys.path.append('F:\eclipse\···\···\test_scrapy_spider')  #设置工程的目录  #复制工程test_scrapy_spider的路径
+
+import sys
+import os
+# os.path.abspath(__file__)  #获取当前文件的路径
+# os.path.dirname(os.path.abspath(__file__))  #获取当前文件的文件夹的路径
+print(os.path.abspath(__file__))  #F:\eclipse\···\···\test_scrapy_spider\main.py
+print(os.path.dirname(os.path.abspath(__file__)))  #F:\eclipse\···\···\test_scrapy_spider
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))  #设置工程的目录
+
+# execute(['scrapy', 'crawl', 'jobbole_spider'])  #调用execute函数，执行scrapy命令
+# execute(['scrapy', 'crawl', 'zhihu'])
+execute(["scrapy", "crawl", "lagou"])
+
 
