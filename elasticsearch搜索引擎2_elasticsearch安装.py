836d04c4aa8ad4cdc91957bf1c8fe7076779f91e
MrCat9 <634467045@qq.com>
2018-04-23 11:49:48 +0800

    update elasticsearch搜索引擎2_elasticsearch安装.py

---
+++ elasticsearch搜索引擎2_elasticsearch安装.py
@@ -0,0 +1,35 @@
+# 1. java sdk安装
+# 2. elasticsearch安装官网下载 不使用官网的版本，提供原始的插件不多
+# 3. elasticsearc-rtf github搜索，中文发行版，已经安装了很多插件 https://github.com/medcl/elasticsearch-rtf
+# 4. 运行elasticsearch的方法，在bin文件目录下进入命令行，执行elasticsearch.bat 
+# 5. 配置文件：elasticsearch-rtf\elasticsearch-rtf-master\config\elasticsearch.yml
+
+C:\Users\admin  #查看java版本
+λ java -version
+
+# 运行elasticsearch
+# cmder下
+C:\Users\admin
+λ f:
+
+F:\cmder\vendor\git-for-windows
+λ cd F:\elasticsearch-rtf-master\bin
+
+F:\elasticsearch-rtf-master\bin
+λ elasticsearch.bat
+
+# elasticsearch 在127.0.0.1：9200上监听  publish_address {127.0.0.1:9200}
+# 打开浏览器  访问127.0.0.1：9200
+{
+name: "I2DdLXU",
+cluster_name: "elasticsearch",
+cluster_uuid: "I_XTNkONQgG-Gb7QIfGsbQ",
+version: {
+number: "5.1.1",
+build_hash: "5395e21",
+build_date: "2016-12-06T12:36:15.409Z",
+build_snapshot: false,
+lucene_version: "6.3.0"
+},
+tagline: "You Know, for Search"
+}
